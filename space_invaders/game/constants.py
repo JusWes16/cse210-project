071b275fe60ef4d4e382baf0e02af329dcdc279d
unknown <jakeard@byui.edu>
2021-06-28 09:39:45 -0700

    took out of project folder folders

---
+++ space_invaders/game/constants.py
@@ -0,0 +1,21 @@
+import os
+
+MAX_X = 800
+MAX_Y = 600
+
+LASER_SPEED = 12
+LASER_Y = MAX_Y / 2
+
+SHIP_Y = 25
+
+SHIP_MOVE_SCALE = 10
+
+ALIEN_WIDTH = 25
+ALIEN_HEIGHT = 15
+ALIEN_SPACE = 10
+
+LASER_IMAGE = "space_invaders/assets/images/laser.png"
+SHIP_IMAGE = "space_invaders/assets/images/spaceship.png"
+ALIEN_IMAGE = "space_invaders/assets/images/alien.png"
+
+
