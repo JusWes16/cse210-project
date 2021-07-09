c6d2c121478d21becf52ecb2a6b1d4b3d04dae6b
Caleb Hall <caleb.mea.hall@gmail.com>
2021-07-09 13:39:42 -0600

    alien lasers kill ship

--- space_invaders/game/handle_collisions_action.py
+++ space_invaders/game/handle_collisions_action.py
@@ -23,6 +23,7 @@ class HandleCollisionsAction(Action):
 
         for laser in cast["lasers"]:
             aliens = cast["aliens"]
+            self._handle_enemy_laser_collision(lasers, laser, ship)
             self._handle_alien_collision(lasers, laser, aliens)
     
     def _handle_walls_collision(self, ship):
@@ -36,6 +37,20 @@ class HandleCollisionsAction(Action):
         elif ship.center_y > 200:
             ship.center_y = 200
         
+    def _handle_enemy_laser_collision(self, lasers, laser, ship):
+        laser_to_remove = None
+        
+        # score = 0
+
+        
+        if laser.collides_with_sprite(ship) and laser.change_y < 0:
+            laser_to_remove = laser
+            ship.remove_life()
+            # score += 50
+            # Score().get_score(score)
+        
+        if laser_to_remove != None:
+            lasers.remove(laser_to_remove)
 
     def _handle_alien_collision(self, lasers, laser, aliens):
         laser_to_remove = None
